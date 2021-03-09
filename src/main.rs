20ee9f918fe5dcf8a64715dfc0fb0717779e0837
stsysd <000stsysd000@gmail.com>
2021-03-10 01:32:42 +0900

    add functionalities
    - multiple tasks in a config file
    - string formatting

--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,42 @@ extern crate serde_derive;
 use reqwest::Method;
 use serde::de::{self, Error, MapAccess, Visitor};
 use serde::{Deserialize, Deserializer};
-use std::collections::BTreeMap;
+use std::collections::{BTreeMap, HashMap};
 use std::fmt;
 use std::fs;
 use std::io::{stdout, BufWriter, Write};
+use strfmt::strfmt;
 use structopt::StructOpt;
 
 #[derive(Debug, Deserialize)]
-struct Req {
+#[serde(untagged)]
+enum ReqConfig {
+    ReqSingle(ReqSingle),
+    ReqTable(ReqTable),
+}
+
+#[derive(Debug, Deserialize)]
+struct ReqTable {
+    req: BTreeMap<String, ReqTask>,
+    #[serde(default = "empty_hash_map")]
+    values: HashMap<String, String>,
+}
+
+#[derive(Debug, Deserialize)]
+struct ReqSingle {
     #[serde(flatten)]
-    method_and_url: ReqMethodAndUrl,
-    #[serde(default = "empty_map")]
+    req: ReqTask,
+    #[serde(default = "empty_hash_map")]
+    values: HashMap<String, String>,
+}
+
+#[derive(Debug, Deserialize)]
+struct ReqTask {
+    #[serde(flatten)]
+    method_and_url: MethodAndUrl,
+    #[serde(default = "empty_tree_map")]
     headers: BTreeMap<String, ReqValue>,
-    #[serde(default = "empty_map")]
+    #[serde(default = "empty_tree_map")]
     queries: BTreeMap<String, ReqValue>,
     #[serde(deserialize_with = "de_body", default)]
     body: Option<ReqBody>,
@@ -26,43 +49,62 @@ struct Req {
 
 #[derive(Debug, Deserialize)]
 #[serde(untagged)]
-enum ReqMethodAndUrl {
-    Specific(ReqMethodAndUrlSp),
+enum ReqValue {
+    Atom(RawOrFmt),
+    List(Vec<RawOrFmt>),
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(untagged)]
+enum RawOrFmt {
+    Raw(String),
+    Fmt { fmt: String },
+}
+
+impl RawOrFmt {
+    fn render(&self, ctxt: &HashMap<String, String>) -> String {
+        match self {
+            RawOrFmt::Raw(s) => s.clone(),
+            RawOrFmt::Fmt { fmt } => strfmt(&fmt, ctxt).expect("fail to render fmt"),
+        }
+    }
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(untagged)]
+enum MethodAndUrl {
+    Specific(MethodAndUrlSp),
     General {
-        url: String,
+        url: RawOrFmt,
         #[serde(deserialize_with = "de_method")]
         method: Option<Method>,
     },
 }
 
 #[derive(Debug, Deserialize)]
-#[serde(untagged)]
-enum ReqValue {
-    Single(String),
-    Multiple(Vec<String>),
-}
-
-#[derive(Debug, Deserialize)]
 #[serde(rename_all = "UPPERCASE")]
-enum ReqMethodAndUrlSp {
-    Get(String),
-    Post(String),
-    Put(String),
-    Delete(String),
+enum MethodAndUrlSp {
+    Get(RawOrFmt),
+    Post(RawOrFmt),
+    Put(RawOrFmt),
+    Delete(RawOrFmt),
 }
 
-impl ReqMethodAndUrl {
-    fn split(self) -> (Method, String) {
+impl MethodAndUrl {
+    fn split(&self) -> (Method, &RawOrFmt) {
         match self {
-            Self::General { url, method: None } => (Method::GET, url),
             Self::General {
-                url,
-                method: Some(m),
-            } => (m, url),
-            Self::Specific(ReqMethodAndUrlSp::Get(url)) => (Method::GET, url),
-            Self::Specific(ReqMethodAndUrlSp::Post(url)) => (Method::POST, url),
-            Self::Specific(ReqMethodAndUrlSp::Put(url)) => (Method::PUT, url),
-            Self::Specific(ReqMethodAndUrlSp::Delete(url)) => (Method::DELETE, url),
+                ref url,
+                method: None,
+            } => (Method::GET, url),
+            Self::General {
+                ref url,
+                method: Some(ref m),
+            } => (m.clone(), url),
+            Self::Specific(MethodAndUrlSp::Get(ref url)) => (Method::GET, url),
+            Self::Specific(MethodAndUrlSp::Post(ref url)) => (Method::POST, url),
+            Self::Specific(MethodAndUrlSp::Put(ref url)) => (Method::PUT, url),
+            Self::Specific(MethodAndUrlSp::Delete(ref url)) => (Method::DELETE, url),
         }
     }
 }
@@ -70,11 +112,11 @@ impl ReqMethodAndUrl {
 #[derive(Debug, Deserialize)]
 enum ReqBody {
     #[serde(rename = "plain")]
-    PlainBody(String),
+    PlainBody(RawOrFmt),
     #[serde(rename = "json")]
     JsonBody(toml::Value),
     #[serde(rename = "form")]
-    FormBody(BTreeMap<String, String>),
+    FormBody(BTreeMap<String, RawOrFmt>),
 }
 
 fn toml_to_json(src: &toml::Value) -> serde_json::Value {
@@ -99,28 +141,32 @@ fn toml_to_json(src: &toml::Value) -> serde_json::Value {
     }
 }
 
-impl Req {
-    fn exec(self) -> reqwest::Result<reqwest::blocking::Response> {
+impl ReqTask {
+    fn exec(&self, ctxt: &HashMap<String, String>) -> reqwest::Result<reqwest::blocking::Response> {
         let (method, url) = self.method_and_url.split();
-        let mut builder = reqwest::blocking::Client::new().request(method, url.as_str());
+        let mut builder = reqwest::blocking::Client::new().request(method, &url.render(ctxt));
         let q = self
             .queries
             .iter()
             .flat_map(|(key, val)| match val {
-                ReqValue::Single(s) => vec![(key.as_str(), s.as_str())],
-                ReqValue::Multiple(v) => v.iter().map(|s| (key.as_str(), s.as_str())).collect(),
+                ReqValue::Atom(s) => vec![(key.as_str(), s.render(ctxt))],
+                ReqValue::List(v) => v.iter().map(|s| (key.as_str(), s.render(ctxt))).collect(),
             })
-            .collect::<Vec<(&str, &str)>>();
+            .collect::<Vec<(&str, String)>>();
         builder = builder.query(&q);
-        builder = match self.body {
-            Some(ReqBody::PlainBody(s)) => builder.body(s),
-            Some(ReqBody::JsonBody(v)) => builder.json(&toml_to_json(&v)),
-            Some(ReqBody::FormBody(m)) => builder.form(&m),
+        builder = match &self.body {
+            Some(ReqBody::PlainBody(s)) => builder.body(s.render(ctxt)),
+            Some(ReqBody::JsonBody(v)) => builder.json(&toml_to_json(v)),
+            Some(ReqBody::FormBody(m)) => builder.form(
+                &m.iter()
+                    .map(|(k, v)| (k, v.render(ctxt)))
+                    .collect::<BTreeMap<&String, String>>(),
+            ),
             None => builder,
         };
         let h = self.headers.iter().flat_map(|(key, val)| match val {
-            ReqValue::Single(s) => vec![(key.as_str(), s.as_str())],
-            ReqValue::Multiple(v) => v.iter().map(|s| (key.as_str(), s.as_str())).collect(),
+            ReqValue::Atom(s) => vec![(key.as_str(), s.render(ctxt))],
+            ReqValue::List(v) => v.iter().map(|s| (key.as_str(), s.render(ctxt))).collect(),
         });
         builder = builder.header(
             "User-Agent",
@@ -163,7 +209,7 @@ where
         where
             E: Error,
         {
-            Ok(ReqBody::PlainBody(value.to_string()))
+            Ok(ReqBody::PlainBody(RawOrFmt::Raw(value.to_string())))
         }
 
         fn visit_map<M>(self, map: M) -> Result<ReqBody, M::Error>
@@ -179,8 +225,12 @@ where
         .map(|v| Some(v))
 }
 
-fn empty_map<T>() -> BTreeMap<String, T> {
+fn empty_tree_map<T>() -> BTreeMap<String, T> {
     BTreeMap::new()
+}
+
+fn empty_hash_map<T>() -> HashMap<String, T> {
+    HashMap::new()
 }
 
 #[derive(Debug, StructOpt)]
@@ -191,15 +241,24 @@ struct Opt {
 
     #[structopt(short = "i", long = "include")]
     include: bool,
+
+    #[structopt(short = "k", long = "key", default_value = "default")]
+    key: String,
 }
 
 fn main() -> std::io::Result<()> {
     let opt = Opt::from_args();
     let input = fs::read_to_string(opt.input.as_str())
         .expect(format!("cannot read file {}", opt.input).as_str());
-    let req = toml::from_str::<Req>(input.as_str())
+    let req = toml::from_str::<ReqConfig>(input.as_str())
         .expect(format!("cannot parse config file {}", opt.input).as_str());
-    let ret = req.exec();
+    let ret = match &req {
+        ReqConfig::ReqSingle(ReqSingle { req, values }) => req.exec(values),
+        ReqConfig::ReqTable(ReqTable { req, values }) => req
+            .get(&opt.key)
+            .expect(format!("cannot find task <{}>", opt.key).as_str())
+            .exec(values),
+    };
     match ret {
         Err(e) => eprintln!("{}", e),
         Ok(res) => {
